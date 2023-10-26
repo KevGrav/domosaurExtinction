e0f1b06723e5b24e42dc6ce438149bcd302b4b09
KevGrav <kgravell.prsvr@gmail.com>
2023-10-26 11:46:37 -0400

    after corrections

--- main.js
+++ main.js
@@ -9,13 +9,15 @@ hideMePic.style.display = "none";
 
 const Img1 = document.getElementById('triceratops');
 Img1.style.width = '324px';
+
 //1
-messWithMeWord.addEventListener('click', function(){
-    messWithMeWord.style.color = 'orange';
+const turnOrange = document.querySelector('span.mess-with-me');
+turnOrange.addEventListener('click', function(){
+    turnOrange.style.color = 'orange';
 })
 //2
 Img1.addEventListener('click', function(){
-    Img1.style.borderImageColor = 'red';
+    Img1.style.border = '5px solid red';
 }
 )
 
@@ -27,14 +29,40 @@ ImgFeathered.addEventListener('click', function() {
 
 //4
 const changeBackgroundColor = document.getElementById('toggle');
-const dinoRow = document.querySelector('div')
+const row = document.getElementById('row')
 changeBackgroundColor.addEventListener('click', function(){
-    dinoRow.style.backgroundColor = 'aqua';
+    row.style.backgroundColor = 'aqua';
 })
 
 //5
-const imgExpand = document.getElementById('biggify');
-imgExpandchange.addEventListener('hover', function(){
-    imgExpand.style.width = '200px';
-})
+const dino = document.getElementById("biggify");
+const growUp=function(){
+  this.style.width="200px";};
+// const shrink=function(){
+//   this.style.width="";};
+dino.addEventListener("mouseover",growUp);
+// dino.addEventListener("mouseout",shrink);
 
+//stretch 1
+// const toggle = document.querySelector('#toggle');
+// const row = document.querySelector('#row');
+// toggle.addEventListener('click', toggleRowBackground)
+// const toggleRowBackground = function() {
+//     if (row.style.backgroundColor === 'black') {
+//       row.style.backgroundColor = 'white';
+//     } else {
+//       row.style.backgroundColor = 'black';
+//     }
+// }
+
+//stretch 2
+// const dinow = document.querySelector('#biggify');
+// dinow.addEventListener('mouseover', toggleSize);
+// dinow.addEventListener('mouseout', toggleSize);
+// const toggleSize = function() {
+//     if (dinow.style.width === '200px') {
+//       dinow.style.width = '162px';
+//     } else {
+//       dinow.style.width = '200px';
+//     }
+//   }
